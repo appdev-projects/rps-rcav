575b74b9455a8b8e11c7f1385fd43e61abbd1d5e
sebgomez2023 <sebgomez@uchicago.edu>
2023-01-23 20:18:45 +0000

    Made Rock page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,12 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
+  def play_rock
+    # render({:html => "<h1>Howdy, world!</h1>".html_safe})
 
+    # redirect_to("https://wikipedia.org")
+
+    render({:template => "game_templates/user_rock.html.erb"})
+
+  end
 end
